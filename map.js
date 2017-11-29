ecf78415fbddd00ba993d281dfb02664fab5503e
jkxm <doofus1102@gmail.com>
2017-11-28 22:57:17 -0500

    images and links

--- map.js
+++ map.js
@@ -73,6 +73,7 @@
 				 	Time:d.Time,
 				 	SentimentScore:d.SentimentScore,
 				 	Link:d.Link,
+				 	Images:d.Images,
 				 }})
 	.get(function(error, rows) {
   		csv.push(rows);
@@ -152,22 +153,28 @@
 			d3.selectAll("p").remove();
 			d3.selectAll("input").remove();
 			d3.selectAll("circle").remove();
+			d3.selectAll("img").remove();
+			d3.selectAll("a").remove();
 
 		}
 		else if(arr.length == 2){
 			retrieveRelation(arr[0].name, arr[1].name);
+			console.log(data[0].Images);
 			var index;
 			//console.log(arr[0].x+" "+arr[1].x);
 			var val = Math.random();
 			var hexString = val.toString(16);
 			var infodiv = d3.select(".information").append("div");
-			
-				infodiv.append("p").text(arr[0].name + " relationship status with " + arr[1].name);
-				//infodiv.append("p").text(arr[1].name);
 				infodiv.append("input").attr("type", "range").attr("min", 1).attr("max", data.length).attr("id", "slider").attr("class", "slider");
+				//infodiv.append("p").text(arr[0].fullname + " relationship status with " + arr[1].fullname);
+				infodiv.append("p").text("During " + data[data.length-1].Time + ", " + arr[0].fullname + " relation with " + arr[1].fullname + " has a score of " + data[data.length-1].SentimentScore)
+				//infodiv.append("p").text(arr[1].name);
 				infodiv.append("p").text(data[data.length-1].SentimentScore).attr("class", "time_score"); // show the sentiment score
 				infodiv.append("p").text(data[data.length-1].Time).attr("class", "time_score");
+				
 				infodiv.append("span").attr("id", "output");
+				infodiv.append("img").attr("src", data[data.length-1].Images);
+				infodiv.append("a").attr("href", data[data.length-1].Link).text("Link to wiki article").attr("target", "_blank");
 			var col = GreenYellowRed(data[data.length-1].SentimentScore);
 			
 			var slope = -(arr[1].y - arr[0].y)/(arr[1].x - arr[0].x);
@@ -234,8 +241,19 @@
 			slider.oninput = function() {
 				d3.selectAll(".time_score").remove();
 				d3.selectAll("line").remove();
-				infodiv.append("p").text(data[this.value - 1].SentimentScore).attr("class", "time_score");
-				infodiv.append("p").text(data[this.value-1].Time).attr("class", "time_score");
+				d3.selectAll("a").remove();
+				d3.selectAll("p").remove();
+				d3.selectAll("img").remove();
+
+				infodiv.append("p").text("During " + data[this.value - 1].Time + ", " + arr[0].fullname + " relation with " + arr[1].fullname + " has a score of " +data[this.value - 1].SentimentScore)
+				//infodiv.append("p").text(arr[1].name);
+				infodiv.append("p").text(data[this.value - 1].SentimentScore).attr("class", "time_score"); // show the sentiment score
+				infodiv.append("p").text(data[this.value - 1].Time).attr("class", "time_score");
+				
+				infodiv.append("span").attr("id", "output");
+				infodiv.append("img").attr("src", data[this.value-1].Images);
+				infodiv.append("a").attr("href", data[this.value - 1].Link).text("Link to wiki article").attr("target", "_blank");
+
 				d3.select("g").append("line")
 					.style("stroke", GreenYellowRed(data[this.value - 1].SentimentScore))
 					.attr("stroke-width", "3")
@@ -278,6 +296,7 @@
 		var countries = topojson.feature(data, data.objects.countries1).features;
 		console.log(countries);
 
+
 		var paths = svg.selectAll(".country")
 			.data(countries)
 			.enter().append("path");
@@ -288,7 +307,7 @@
 			.on("click", function(d){ //push coordinates onto the selected countries
 				var pos = d3.mouse(this);
 				console.log(pos[0]+" "+pos[1]);
-				selectedCountries.push({name:d.id, x:pos[0], y:pos[1]});
+				selectedCountries.push({name:d.id, x:pos[0], y:pos[1], fullname:d.properties.name});
 				drawLine(selectedCountries);
 			})
 			.on("mouseover", function(d){d3.select(this).classed("hovering", true);})
